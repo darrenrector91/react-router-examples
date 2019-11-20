84f53975eec862969678623c54f84eb6fbf5bde7
Darren <dgrector55428@gmail.com>
2019-11-19 21:28:36 -0600

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { Route, Link, BrowserRouter as Router } from "react-router-dom";
+import App from "./App";
+import Users from "./Users";
+import Contact from "./Contact";
+import "./index.css";
+const routing = (
+  <Router>
+    <div>
+      <ul className="menu">
+        <li>
+          <Link className="links" to="/">
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link className="links" to="/users">
+            Users
+          </Link>
+        </li>
+        <li>
+          <Link className="links" to="/contact">
+            Contact
+          </Link>
+        </li>
+      </ul>
+      <Route exact path="/" component={App} />
+      <Route path="/users" component={Users} />
+      <Route path="/contact" component={Contact} />
+    </div>
+  </Router>
+);
+ReactDOM.render(routing, document.getElementById("root"));
